ba83a9460b2c16e903ef0777963bc50e4d84fa12
Maggie Shan <sshan.maggie@gmail.com>
2019-07-24 10:03:55 -0400

    Added gallery images + edited tabbed menu

--- script.js
+++ script.js
@@ -2,32 +2,32 @@
 var panel = document.getElementsByClassName("panel");
 function togglePanel() {
     if (event.target.previousElementSibling) {
-        if (event.target.previousElementSibling.style.maxHeight) {
-            event.target.previousElementSibling.style.maxHeight = null;
+        if (event.target.previousElementSibling.style.minHeight) {
+            event.target.previousElementSibling.style.minHeight = null;
             event.target.previousElementSibling.previousElementSibling.classList.remove("tabActive");
         }
         if (event.target.previousElementSibling.previousElementSibling.previousElementSibling) {
-            if (event.target.previousElementSibling.previousElementSibling.previousElementSibling.style.maxHeight) {
-                event.target.previousElementSibling.previousElementSibling.previousElementSibling.style.maxHeight = null;
+            if (event.target.previousElementSibling.previousElementSibling.previousElementSibling.style.minHeight) {
+                event.target.previousElementSibling.previousElementSibling.previousElementSibling.style.minHeight = null;
                 event.target.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.classList.remove("tabActive");
             }
         }
     }
     if (event.target.nextElementSibling.nextElementSibling) {
-        if (event.target.nextElementSibling.nextElementSibling.nextElementSibling.style.maxHeight) {
-            event.target.nextElementSibling.nextElementSibling.nextElementSibling.style.maxHeight = null;
+        if (event.target.nextElementSibling.nextElementSibling.nextElementSibling.style.minHeight) {
+            event.target.nextElementSibling.nextElementSibling.nextElementSibling.style.minHeight = null;
             event.target.nextElementSibling.nextElementSibling.classList.remove("tabActive");
         }
         if (event.target.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling) {
-            event.target.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.style.maxHeight = null;
+            event.target.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.style.minHeight = null;
             event.target.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.classList.remove("tabActive");
         }
     } 
-    if(event.target.nextElementSibling.style.maxHeight) {
-        event.target.nextElementSibling.style.maxHeight = null;
+    if(event.target.nextElementSibling.style.minHeight) {
+        event.target.nextElementSibling.style.minHeight = null;
         event.target.classList.remove("tabActive");
     } else {
-        event.target.nextElementSibling.style.maxHeight = event.target.scrollHeight + 50 + "px";
+        event.target.nextElementSibling.style.minHeight = "150px";
         event.target.nextElementSibling.classList.add("openPanel");event.target.classList.add("tabActive");
     }
 
