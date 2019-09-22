f99d708f54fbb5835b14960f4516bc10282bf478
Maggie Shan <sshan.maggie@gmail.com>
2019-09-22 18:21:17 -0400

    Updated script

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# MaggieShan.github.io
-Check out my portfolio website here (still in the making): https://maggieshan.github.io/
-
+Check out my portfolio website here: https://maggieshan.github.io/
+- features both my deisgn and coding projects
+- main medium I use to improve my web development skills (exploring different JS libraries, using tools like bootstrap, and other specifics on how I improve responsiveness and accessibility) 
--- script.js
+++ script.js
@@ -2,32 +2,30 @@
 // To make sure a tab opens when clicked, while other active ones close
 var panel = document.getElementsByClassName("panel");
 function togglePanel() {
-    // If there exists a tab before the current one
-    if (event.target.previousElementSibling) {
-        event.target.previousElementSibling.style.minHeight = null;
-        event.target.previousElementSibling.previousElementSibling.classList.remove("tabActive");
-        // If there exists 2 tabs before the current one 
-        if (event.target.previousElementSibling.previousElementSibling.previousElementSibling) {
-            event.target.previousElementSibling.previousElementSibling.previousElementSibling.style.minHeight = null;
-            event.target.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.classList.remove("tabActive");
-        }
+    var accordians = document.getElementsByClassName("accordian");
+    var tabClicked = event.target; 
+    var tabPanel = tabClicked.nextElementSibling; 
+
+    // Open current panel 
+    tabPanel.style.minHeight = "150px";
+    tabPanel.classList.add("openPanel");
+    tabPanel.classList.add("tabActive");
+
+    //Close all others 
+    if (tabClicked == accordians[0]) { // Development 
+        close(accordians[1]);
+        close(accordians[2]); 
+    } else if (tabClicked == accordians[1]) { // Design
+        close(accordians[0]);
+        close(accordians[2]); 
+    } else { // Languages
+        close(accordians[0]);
+        close(accordians[1]); 
     }
-    // If there exists a tab after the current one
-    if (event.target.nextElementSibling.nextElementSibling) {
-        event.target.nextElementSibling.nextElementSibling.nextElementSibling.style.minHeight = null;
-        event.target.nextElementSibling.nextElementSibling.classList.remove("tabActive");
-        // If there there exists 2 tabs after the current one
-        if (event.target.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling) {
-            event.target.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.style.minHeight = null;
-            event.target.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.classList.remove("tabActive");
-        }
-    } 
-    // If current tab is already open
-    if(event.target.nextElementSibling.style.minHeight) {
-        event.target.nextElementSibling.style.minHeight = null;
-        event.target.classList.remove("tabActive");
-    } else {
-        event.target.nextElementSibling.style.minHeight = "150px";
-        event.target.nextElementSibling.classList.add("openPanel");event.target.classList.add("tabActive");
-    }
+}
+
+// Closes the given tab 
+function close(tab) {
+    tab.nextElementSibling.style.minHeight = null; 
+    tab.nextElementSibling.classList.remove("tabActive");
 }
